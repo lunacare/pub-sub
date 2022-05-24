25eb62ae8da667dfc9eb1b05c1503601472f7c0e
Alex Bruns <alex.h.bruns@gmail.com>
2022-05-24 17:23:25 -0500

    clean up
    Signed-off-by: Alex Bruns <alex.h.bruns@gmail.com>

--- lib/pub_sub/class_forwarding.rb
+++ lib/pub_sub/class_forwarding.rb
@@ -2,9 +2,7 @@ module PubSub
   def self.class_forwarding
     Module.new do
       def initialize(...)
-        p "in initialize"
         add_subscriber(self.class)
-        p "doing base initialize"
         super(...)
       end
     end
