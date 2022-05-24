92689d42fed96a3ea508959ec7886e1f0d340c32
Alex Bruns <alex.h.bruns@gmail.com>
2022-05-24 16:49:43 -0500

    fix assignment
    Signed-off-by: Alex Bruns <alex.h.bruns@gmail.com>

--- lib/pub_sub/event.rb
+++ lib/pub_sub/event.rb
@@ -2,9 +2,9 @@ module PubSub
   class Event
     attr_accessor :name, :payload
 
-    def initialize(event_name, event_payload)
-      name = event_name
-      payload = event_payload
+    def initialize(name, payload)
+      @name = name
+      @payload = payload
     end
   end
 end
